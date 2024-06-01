19e60f8dd7a1200270ceee5d67e5e507ce0701fa
sevdevelop1 <sevdevelop@gmail.com>
2024-06-01 12:18:25 +0100

    FEATURE: Form correctly

--- main.js
+++ main.js
@@ -1,10 +1,41 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
+  import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
-    alert(`Gracias por registrarte, ${name}! Te hemos enviado un correo de confirmación a ${email}.`);
-    
-    // Aquí puedes agregar código para enviar los datos del formulario a un servidor
+let loading = false;
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCfZOEwgAphf0XL66oaoDtkgAKeQKlLmf4",
+    authDomain: "contactform-90103.firebaseapp.com",
+    databaseURL: "https://contactform-90103-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "contactform-90103",
+    storageBucket: "contactform-90103.appspot.com",
+    messagingSenderId: "408888185041",
+    appId: "1:408888185041:web:58fb61a2d1f940e3dcf5f0"
+  };
+
+
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+document.getElementById('registerForm').addEventListener('submit', async function(event) {
+    event.preventDefault();
+    document.getElementById('text-submit').style.visibility='hidden';
+    document.getElementById('loader').style.visibility='visible';    
+    try {
+        const docRef = await addDoc(collection(db, "users"), {
+          name: document.getElementById('name').value,
+          email: document.getElementById('email').value
+        });
+        console.log("Document written with ID: ", docRef.id);
+
+        if (docRef) {
+            loading = false;
+            window.location.href = 'gracias';
+        }
+      } catch (e) {
+        console.error("Error adding document: ", e);
+        loading=false;
+      }
 });
