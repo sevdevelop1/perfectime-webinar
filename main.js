b9051cdd66343d07e06067492ccb9329f11d582e
sevdevelop1 <sevdevelop@gmail.com>
2024-06-02 16:23:55 +0100

    ADD: Emailjs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-app.js";
-  import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
+import { getFirestore, addDoc, collection } from "https://www.gstatic.com/firebasejs/9.4.0/firebase-firestore.js";
 
 let loading = false;
 
@@ -15,27 +15,49 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
-
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const email = emailjs.init("Z1jyCZozX_0RWb16t");
 
 document.getElementById('registerForm').addEventListener('submit', async function(event) {
     event.preventDefault();
+    document.getElementById('submit').setAttribute("disabled","disabled");
+    document.getElementById('submit').classList.add("disabled");
     document.getElementById('text-submit').style.visibility='hidden';
     document.getElementById('loader').style.visibility='visible';    
     try {
+        const nameClient = document.getElementById('name').value;
+        const emailClient = document.getElementById('email').value;
         const docRef = await addDoc(collection(db, "users"), {
-          name: document.getElementById('name').value,
-          email: document.getElementById('email').value
+          name: nameClient,
+          email: emailClient
         });
         console.log("Document written with ID: ", docRef.id);
 
         if (docRef) {
-            loading = false;
-            window.location.href = 'gracias';
+          emailjs
+            .send('default_service', 'template_x2v921v', {
+              name: nameClient,
+              reply_to: emailClient,
+            })
+            .then(
+              (response) => {
+                loading = false;
+                document.getElementById('submit').removeAttribute("disabled");
+                document.getElementById('submit').classList.remove("disabled");
+                window.location.href = 'gracias';
+              },
+              (err) => {
+                console.log(err);
+                document.getElementById('submit').removeAttribute("disabled");
+                document.getElementById('submit').classList.remove("disabled");
+              }
+            );
         }
       } catch (e) {
         console.error("Error adding document: ", e);
         loading=false;
+        document.getElementById('submit').removeAttribute("disabled");
+        document.getElementById('submit').classList.remove("disabled");
       }
 });
